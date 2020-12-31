ca2d0b574ec1164e9e20e49cb7cdca165d33235d
Landon Turner <landonlewisturner@gmail.com>
2020-12-31 18:51:09 -0500

    retooled name functions to be more reusable and IP agnostic

--- api/name_functions.py
+++ api/name_functions.py
@@ -1,16 +1,18 @@
-# pre project sandbox for functions to use in name generation endpoints
+altmer_surname_a = ['Ad', 'Caem', 'Elsin', 'Gae', 'Gray', 'High', 'Jor', 'Lareth', 'Silin', 'Spell', 'Storm', 'Throm']
+altmer_surname_b = ['aire', 'al', 'binder', 'ian', 'ire', 'ius', 'lock', 'or', 'orin', 'thar', 'us', 'watch']
 
-name_cache = {} 
 
-altmer_a = ['Ad', 'Caem', 'Elsin', 'Gae', 'Gray', 'High', 'Jor', 'Lareth', 'Silin', 'Spell', 'Storm', 'Throm']
-altmer_b = ['aire', 'al', 'binder', 'ian', 'ire', 'ius', 'lock', 'or', 'orin', 'thar', 'us', 'watch']
-
-def create_surnames_elder_scrolls(front_half_list, back_half_list):
+def bulk_create_names_from_halves(front_half_list, back_half_list):
     ''' Takes two lists, combines each half into every possible name combination.'''
-    list_index = 0
+    
+    generated_names = []
     for partial_name in front_half_list:
-        generated_surname = partial_name + back_half_list[list_index]
-        if generated_surname not in name_cache:
-            name_cache[generated_surname] = 'exists'
+        names = [partial_name + string for string in back_half_list]
+        generated_names.append(names)
+    
+    # squash the list of lists
+    generated_names = [name for sublist in generated_names for name in sublist]
+
+    return generated_names
 
 
