3dda39d86023906d5c1a0b4a3bb62dc84d2cb1bf
Landon Turner <landonlewisturner@gmail.com>
2020-12-31 18:57:33 -0500

    break out name files

---
+++ api/elder_scrolls_data.py
@@ -0,0 +1,7 @@
+''' File to hold the basic naming convention data for the Elder Scrolls universe '''
+
+# ALTMER NAMES
+
+## Surnames
+altmer_surname_a = ['Ad', 'Caem', 'Elsin', 'Gae', 'Gray', 'High', 'Jor', 'Lareth', 'Silin', 'Spell', 'Storm', 'Throm']
+altmer_surname_b = ['aire', 'al', 'binder', 'ian', 'ire', 'ius', 'lock', 'or', 'orin', 'thar', 'us', 'watch']
--- api/name_functions.py
+++ api/name_functions.py
@@ -1,11 +1,9 @@
-altmer_surname_a = ['Ad', 'Caem', 'Elsin', 'Gae', 'Gray', 'High', 'Jor', 'Lareth', 'Silin', 'Spell', 'Storm', 'Throm']
-altmer_surname_b = ['aire', 'al', 'binder', 'ian', 'ire', 'ius', 'lock', 'or', 'orin', 'thar', 'us', 'watch']
-
 
 def bulk_create_names_from_halves(front_half_list, back_half_list):
     ''' Takes two lists, combines each half into every possible name combination.'''
     
     generated_names = []
+
     for partial_name in front_half_list:
         names = [partial_name + string for string in back_half_list]
         generated_names.append(names)
